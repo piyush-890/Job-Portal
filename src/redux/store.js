a95eb59b5d66a53a452c0affd534e7ed331e4e4e
piyush-890 <pp257132@gmail.com>
2026-07-06 00:28:24 +0530

    Add files via upload

---
+++ src/redux/store.js
@@ -0,0 +1,56 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+import jobSlice from "./jobSlice";
+import companySlice from "./companySlice";
+import applicationSlice from "./applicationSlice";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import storageModule from "redux-persist/lib/storage";
+
+const storage = storageModule.default ?? storageModule;
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const rootReducer = combineReducers({
+  auth: authSlice,
+  job: jobSlice,
+  company: companySlice,
+  application: applicationSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          FLUSH,
+          REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER,
+        ],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
